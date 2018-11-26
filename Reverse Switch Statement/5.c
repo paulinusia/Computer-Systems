dafc774cd8682eb690532843d7e529f3c554ffcf
paulinusia <paulina.adamski@gmail.com>
2018-11-25 21:56:09 -0600

    Add files via upload

---
+++ Reverse Switch Statement/5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+Long switch prob(long x, long n){
+Long result = x;
+switch(n){
+	case 50:
+	
+	case 52:
+		result *= 4+0;
+		break;
+	case 53:
+		result >>=2;
+		break;
+	case 54:
+		result *-3;
+	case 55:
+		result *= result;
+	default:
+		result += 10;
+
